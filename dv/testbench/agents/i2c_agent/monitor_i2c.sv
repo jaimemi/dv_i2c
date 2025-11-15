964f0b51d86d02d2862a76f481cf227d13848113
jaimemi <j.miguelatanes@gmail.com>
2025-11-15 14:13:37 +0100

    monitor hecho

--- dv/testbench/agents/i2c_agent/monitor_i2c.sv
+++ dv/testbench/agents/i2c_agent/monitor_i2c.sv
@@ -12,22 +12,91 @@ class i2c_monitor extends uvm_monitor;
   endfunction : new
 
   function void build_phase(uvm_phase phase);
+    super.build_phase(phase);
     port = new("port", this);
   endfunction : build_phase
   
   function void connect_phase(uvm_phase phase);
-    //assert(uvm_config_db#(virtual dut_if)::get(this, "", "dut_if", dut_vif));  
+    assert(uvm_config_db#(virtual dut_if)::get(this, "", "dut_if", dut_vif));  
   endfunction
   
   task run_phase(uvm_phase phase);
     fork
+      @(posedge dut_vif.reset_n);
       forever begin
-        i2c_basic_tr tr_dummy;
-        tr_dummy = i2c_basic_tr::type_id::create("tr_dummy");
-        port.write(tr_dummy);
+        // Definir variables de lectura
+        i2c_basic_tr tr;
+
+        logic [7:0] byte_addr, byte_reg, byte_data;
+        logic ack1, ack2, ack3;
+
+        tr = i2c_basic_tr::type_id::create("tr");
+
+        // Decodificar datos
+        wait_for_start();
+
+        get_byte(byte_addr);
+        get_ack(ack1);
+      
+        get_byte(byte_reg);
+        get_ack(ack2);
+      
+        get_byte(byte_data);
+        get_ack(ack3);
+
+        wait_for_stop();
+
+        // Rellenar lla transacción con los datos observados
+        tr.device_addr = byte_addr[7:1];
+        tr.read        = byte_addr[0];
+        tr.addr        = byte_reg;
+        tr.data        = byte_data;
+
+        // Linea de datos baja. Esperar.
+        `uvm_info(get_type_name(), $sformatf("Transacción I2C detectada: %s", tr.sprint()), UVM_HIGH)
+        port.write(tr);
+
       end
     join_none;    
   endtask : run_phase
+
+  local task wait_for_start();
+    `uvm_info(get_type_name(), "Monitor: Esperando START", UVM_HIGH)
+    forever begin
+      @(negedge dut_vif.sda_val); // Espera a que SDA baje
+      if (dut_vif.scl_val == 1) begin // Comprueba si SCL estaba alto
+        `uvm_info(get_type_name(), "Monitor: START detectado", UVM_HIGH)
+        break; // ¡Es un START! Salir del bucle.
+      end
+    end
+  endtask
+
+  local task get_byte(output logic [7:0] byte_out);
+    for (int i = 0; i < 8; i++) begin
+      // Los datos se leen en el flanco de subida de SCL
+      @(posedge dut_vif.scl_val);
+      byte_out[7-i] = dut_vif.sda_val;
+      @(negedge dut_vif.scl_val);
+    end
+  endtask
+  
+  local task get_ack(output logic ack);
+    // El ACK se lee en el 9º flanco de subida
+    @(posedge dut_vif.scl_val);
+    ack = dut_vif.sda_val;
+    @(negedge dut_vif.scl_val);
+  endtask
+
+  local task wait_for_stop();
+    `uvm_info(get_type_name(), "Monitor: Esperando STOP", UVM_HIGH)
+    forever begin
+      @(posedge dut_vif.sda_val); // Espera a que SDA suba
+      if (dut_vif.scl_val == 1) begin // Comprueba si SCL estaba alto
+        `uvm_info(get_type_name(), "Monitor: STOP detectado", UVM_HIGH)
+        break; // ¡Es un STOP! Salir del bucle.
+      end
+    end
+  endtask
   
 endclass: i2c_monitor
 
--- dv/testbench/testbench.sv
+++ dv/testbench/testbench.sv
@@ -8,13 +8,34 @@ import uvm_pkg::*;
 module top;
   //dut if instance
   dut_if dut_if();
+
+  logic [7:0] reg_addr_wire;
+  logic       wr1rd0_wire;
+  logic [7:0] reg_wr_data_wire;
+  logic       reg_req_wire;
+  logic [7:0] reg_rd_data_wire = '0; // No hay reg_block
+  logic [6:0] i2c_addr_wire = 7'd1; // Dirección del I2C slave
+
   environment env;
   //dut instance
+  i2c_slave dut(
+    .SDA(dut_if.sdata),
+    .SCL(dut_if.sclk),
+    .rst_n(dut_if.reset_n),
+    .address(i2c_addr_wire),
+    .reg_addr(reg_addr_wire), // La dirección se ha leido correctamente
+    .wr1rd0(wr1rd0_wire), // 1 si la operación es de escritura, 0 si es de lectura
+    .reg_wr_data(reg_wr_data_wire), // Decodifica la info que escribe el maestro por I2C para que se guarde en el reg_block.
+    .reg_rd_data(reg_rd_data_wire), // Obtiene la información del registro
+    .reg_req(reg_req_wire) // Indica al reg_block que puede leer/escribir en un address.
+  );
+  //other instances?
+
+  // Clock
   initial begin
     dut_if.clk = 0;
     forever #1ns dut_if.clk = ~dut_if.clk;
   end
-  //other instances?
   
   initial begin
     $dumpfile("dump.vcd"); $dumpvars;
