a1be718a2d4ce1774cc0670ed6237738f1639628
jaimemi <j.miguelatanes@gmail.com>
2025-12-01 17:18:26 +0100

    Todos los testbench funcionan

--- dv/testbench/agents/i2c_agent/seq_i2c.sv
+++ dv/testbench/agents/i2c_agent/seq_i2c.sv
@@ -4,11 +4,11 @@
 `include "transaction_i2c.sv"
 
 class i2c_basic_seq extends uvm_sequence#(i2c_basic_tr);
-  byte i2c_device_addr; // Master & Slave Addres
+  byte i2c_device_addr = 1; // Master & Slave Addres
   byte i2c_addr;  //register address
   byte i2c_data;  //register value
-  bit i2c_read;   //1: read; 0: write
-  bit i2c_force_abort; // Force message abort
+  bit i2c_read = 0;   //1: read; 0: write
+  bit i2c_force_abort = 0; // Force message abort
   
   `uvm_object_utils(i2c_basic_seq)
   
